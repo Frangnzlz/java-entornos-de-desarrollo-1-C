6453c70febdae9fe39660edc359d3db44cf48e8d
Frangnzlz <151971568+Frangnzlz@users.noreply.github.com>
2024-03-19 17:19:48 +0100

    Triángulo

---
+++ Figura/src/modelos/Triangulo.java
@@ -0,0 +1,69 @@
+package modelos;
+
+
+import java.util.*;
+
+public class Triangulo {
+
+    private int base;
+    private int altura;
+
+    /**
+     * Constructor por defecto
+     */
+    public void Triangulo() {
+        this.base = 1;
+        this.altura = 1;
+    }
+
+    /**
+     * Constructor para indicar la base y la altura
+     * @param base 
+     * @param altura
+     */
+    public void Triangulo(int base, int altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    /**
+     * Método para modificar la base
+     * @param base
+     */
+    public void setBase(int base) {
+        this.base = base;
+    }
+
+    /**
+     * Método para modificar la altura
+     * @param altura
+     */
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    /**
+     * Método para obtener la base
+     * @return
+     */
+    public int getBase() {
+        return base;
+    }
+
+    /**
+     * Método para obtener la altura
+     * @return
+     */
+    public int getAltura() {
+        return altura;
+    }
+
+    /**
+     * Método para obtener el area
+     * @return
+     */
+    public double getArea() {
+        return ((base*altura)/2);
+    }
+
+}
